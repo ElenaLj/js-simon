ae57454b1b12a25b5bd6b1d2abb3c3ceb0343a5d
ElenaLj <elenaljubijankic@gmail.com>
2021-11-24 15:47:30 +0100

    insert 5 num random

--- js/script.js
+++ js/script.js
@@ -2,15 +2,19 @@
 
 //variabile di riferimento al container dei numeri
 const numContainer = document.querySelector(".container");
-console.log(numContainer);
+// console.log(numContainer);
 
 //questa funzione genera un numero casuale da 1 a 100
 function getRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
 }
-//invocare la f.ne
-getRandomNumber();
-console.log(getRandomNumber());
 
-//output HTML
-numContainer.innerHTML = getRandomNumber();
+//invoco la f.ne per generare un numero
+// getRandomNumber();
+// console.log(getRandomNumber());
+
+//invoco la funzione in un ciclo for perché mi servono 5 numeri random
+for (let i = 0; i < 5; i++){
+    // console.log(i);
+    numContainer.innerHTML += getRandomNumber() + " ";
+}
